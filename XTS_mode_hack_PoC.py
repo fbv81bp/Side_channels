5f2dcaf4f2ac9a30702b4a064bd478a0eaf4d8fb
Fekete Balazs Valer <48908633+fbv81bp@users.noreply.github.com>
2026-04-28 01:14:52 +0200

    Add proof of concept for XTS encryption hacking
    Demonstrate a proof of concept for hacking XTS hard disk encryption by analyzing power consumption during encryption. The method exploits Galois multiplication shifts to create autocorrelation in power traces.

--- XTS_mode_hack_PoC.py
+++ XTS_mode_hack_PoC.py
@@ -1,3 +1,10 @@
+'''
+
+shows a proof of concept of hacking XTS hard disk encryption in a way similar to supply penetrating DPA so, that just a specially formatted file needs to be saved on the disk, while the encryption process' power consumption is being measured: it abuses that the Galois multiplication by 2 which is a simple shift on most bytes, which is then capable of creating autocorrelation in the power trace at every 8th shift(!)
+
+'''
+
+
 s_box = [
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
     0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
